19089e0a82ec92c889edfc08648186e4316df46b
Hojimurod <hojimurodsultonov2004@gmail.com>
2023-12-31 20:59:33 +0500

    My first project

---
+++ PDPLastTask/cells/PostCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostCollectionViewCell.swift
+//  PDPLastTask
+//
+//  Created by Khojimurod Sultonov on 31.12.23.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PDPLastTask/models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  PDPLastTask
+//
+//  Created by Khojimurod Sultonov on 31.12.23.
+//
+
+import Foundation
